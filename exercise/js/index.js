defbf20750a0f3e034302a7abb06f5356de3683d
Valeria <valery0007@gmail.com>
2021-01-06 18:37:18 +0100

    input search query

--- exercise/js/index.js
+++ exercise/js/index.js
@@ -1,29 +1,43 @@
 const URL = "http://localhost:3000/tweets";
 
-/**
- * Retrive Twitter Data from API
- */
-const getTwitterData = () => {
 
+const onEnter = (e) => {
+        //console.log(e);
+        if (e.key == "Enter") {
+            getTwitterData();
+        }
+    }
+    /**
+     * Retrive Twitter Data from API
+     */
+const getTwitterData = () => {
+    const query = document.getElementById('user-search-input').value;
+
+    //const url = "http://localhost:3000/tweets?q=coding&count=10";
+
+    if (!query) return;
+    const encodedQuery = encodeURIComponent(query);
+
+    const fullUrl = `${URL}?q=${encodedQuery}&count=10`;
+    fetch(fullUrl).then((response) => { response.json(); }).then((data) => {
+        console.log(data);
+    });
 }
 
 /**
  * Save the next page data
  */
-const saveNextPage = (metadata) => {
-}
+const saveNextPage = (metadata) => {}
 
 /**
  * Handle when a user clicks on a trend
  */
-const selectTrend = (e) => {
-}
+const selectTrend = (e) => {}
 
 /**
  * Set the visibility of next page based on if there is data on next page
  */
-const nextPageButtonVisibility = (metadata) => {
-}
+const nextPageButtonVisibility = (metadata) => {}
 
 /**
  * Build Tweets HTML based on Data from API
