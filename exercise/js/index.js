94e88983c623ad256d23ebe30e5be8326c3d50e8
Valeria <valery0007@gmail.com>
2021-01-09 22:27:53 +0100

    next page

--- exercise/js/index.js
+++ exercise/js/index.js
@@ -1,5 +1,6 @@
 const URL = "http://localhost:3000/tweets";
 
+let nextPageUrl = null;
 
 const onEnter = (e) => {
         //console.log(e);
@@ -21,18 +22,29 @@ const getTwitterData = () => {
     const fullUrl = `${URL}?q=${encodedQuery}&count=10`;
     fetch(fullUrl).then((response) => { response.json(); }).then((data) => {
         buildTweets(data.statuses);
+        saveNextPage(data.search_metadata);
     });
 }
 
 /**
  * Save the next page data
  */
-const saveNextPage = (metadata) => {}
+const saveNextPage = (metadata) => {
+    if (metadata.next_results) {
+        nextPageUrl = `${URL}${metadata.next_results}`;
+    } else {
+        nextPageUrl = null;
+    }
+}
 
 /**
  * Handle when a user clicks on a trend
  */
-const selectTrend = (e) => {}
+const selectTrend = (e) => {
+    const text = e.innerText;
+    document.getElementById('user-search-input').value = text;
+    getTwitterData();
+}
 
 /**
  * Set the visibility of next page based on if there is data on next page
@@ -45,12 +57,14 @@ const nextPageButtonVisibility = (metadata) => {}
 const buildTweets = (tweets, nextPage) => {
     let twitterContent = '';
     tweets.map((tweet) => {
+
+        const createdDate = moment(tweet.created_at).fromNow();
         twitterContent += `<div class="tweet-container">
                     <div class="tweet-user-info">
-                        <div class="tweet-user-profile"></div>
+                        <div class="tweet-user-profile" style="background-image: url(${tweet.user.profile_image_url_https})"></div>
                         <div class="tweet-user-name-container">
-                            <div class="tweet-user-fullname">Valeria Lupsescu</div>
-                            <div class="tweet-user-username">@valnicky</div>
+                            <div class="tweet-user-fullname">${tweet.user.name}</div>
+                            <div class="tweet-user-username">@${tweet.user.screen_name}</div>
                         </div>
                     </div>`
         if (tweet.extended_entities && tweet.extended_entities.media.length > 0) {
@@ -67,7 +81,7 @@ const buildTweets = (tweets, nextPage) => {
         twitterContent += `<div class="tweet-text-container">
                         ${tweet.full_text}
                     </div>
-                    <div class="tweet-date-container">20 hours ago</div>
+                    <div class="tweet-date-container">${{createdDate}</div>
                 </div>`
     })
 
@@ -101,7 +115,11 @@ const buildVideo = (mediaList) => {
     mediaList.map((media) => {
         if (media.type == "video") {
             videoExists = true;
-            videoContent += `<video controls><source src="${media.video_info.variants[0]}" type="video/mp4"></video>`
+            videoContent += `<video controls><source src="${media.video_info.variants[0].url}" type="video/mp4"></video>`
+        } else if (media.type == "animated_gif") {
+            videoExists = true;
+            videoContent += `
+            <video loop autoplay > < source src = "${media.video_info.variants[0].url}" type="video/mp4"> < /video>`
         }
     });
     videoContent += `</div>`;
